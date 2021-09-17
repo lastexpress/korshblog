1b80c9ffac5fb348478ad3d6ce236a35dfa6f0ca
korsh <funplex151201@gmail.com>
2021-09-17 12:17:08 +0400

    proverka2

--- korsh/settings.py
+++ korsh/settings.py
@@ -142,9 +142,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
